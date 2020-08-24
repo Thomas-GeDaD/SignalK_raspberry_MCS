6448bb927ee604a9050e6cd0aa1ccd7c80dd2063
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2020-08-24 17:16:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,28 @@ var Gpio = require('onoff').Gpio;
 var  ttyinterfaces = [];
 
 
-var entry1= "dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600\ndtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600\ndtoverlay=sc16is752-i2c,int_pin=6,addr=0x48,xtal=14745600"
-var entry2= "dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25\ndtoverlay=spi-bcm2835-overlay"
-var entry3= "sudo sh -c \"echo '#physical can interfaces\nallow-hotplug can0\niface can0 can static\nbitrate 250000\ndown /sbin/ip link set $IFACE down\nup /sbin/ifconfig $IFACE txqueuelen 10000' >> /etc/network/interfaces.d/can0\""
+var entry1= "dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600\\ndtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600\\ndtoverlay=sc16is752-i2c,int_pin=6,addr=0x48,xtal=14745600"
+var entry2= "dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25\\ndtoverlay=spi-bcm2835-overlay"
+var entry3= "sudo sh -c \"echo '#physical can interfaces\\nallow-hotplug can0\\niface can0 can static\\nbitrate 250000\\ndown /sbin/ip link set $IFACE down\\nup /sbin/ifconfig $IFACE txqueuelen 10000' >> /etc/network/interfaces.d/can0\""
 
 module.exports = function (app) {
   let plugin = {}
   let timer = null
+  var data = ""
 
   plugin.id = 'SignalK_raspberry_MCS'
   plugin.name = 'Raspberry_MCS Plugin'
   plugin.description = 'SignalK Plugin to provide MCS functionality to SignalK'
 
   //read config.txt entrys
-  const data = fs.readFileSync('/boot/config.txt', 'utf8');
+  data = fs.readFileSync('/boot/config.txt', 'utf8');
+  console.log(data)
   
   //check sc16is752 config.txt entrys 
   function checksc16is752(){
     var sc16is752
   if (data.indexOf(entry1)==-1){
-        sc16is752 = `sudo sh -c \"echo ${entry1} ' >> /boot/config.txt\"`
+        sc16is752 = `sudo sh -c \"echo '${entry1}' >> /boot/config.txt\"`
     }else{
         sc16is752 = "entrys already created. You have nothing to do ;-)"
     }
@@ -32,7 +34,7 @@ module.exports = function (app) {
   function checkmcp2515(){
     var mcp2515
     if (data.indexOf(entry2)==-1){
-      return  `sudo sh -c \"echo ${entry2} ' >> /boot/config.txt\"`
+      return  `sudo sh -c \"echo '${entry2}' >> /boot/config.txt\"`
     }else{
       return  "entrys already created. You have nothing to do ;-)"
     }
@@ -40,6 +42,7 @@ module.exports = function (app) {
     //check can0 interface in interfaces.d
     function checkcan0if(){
         var can0 = fs.readdirSync('/etc/network/interfaces.d/')
+        console.log(can0)
         if (can0.includes("can0")) {
             return "can 0 is already loaded to /etc/network/interfaces.d/"
         }
@@ -69,39 +72,34 @@ module.exports = function (app) {
         "type": "boolean",
         "title": "Active"
         },
-  
+        "title":{
+            "title": "Here you find information how you initial setup your MCS: ",
+            "type":"null"
+        },
         "information1":{
-        "description":"load the tty overlays for use the serial (NMEA0183) inputs to config.txt:",
-        "type": "string",
-        "title": "How to setup (enter this comands to the comandline)",
-        "default": `${checksc16is752()}`,
-        "readOnly": true
+            "title": "Add the SC16ia752 to the config.txt:",
+            "description":`${checksc16is752()}`,
+            "type": "null"
         },
         "information2":{
-        "description":"load the can interface (NMEA2000) to config.txt:",
-        "type": "string",
-        "title": " ",
-        "default": `${checkmcp2515()}`,
-        "readOnly": true
+            "title": "Add the mcp2515 to the config.txt: ",
+            "description":`${checkmcp2515()}`,
+            "type": "null"
         },
         "information3":{
-        "description":"load the can interface to interface.d:",
-        "type": "string",
-        "title": " ",
-        "default": `${checkcan0if()}`,
-        "readOnly": true
+            "title": "Load the can interface to interface.d: ",
+            "description":`${checkcan0if()}`,
+            "type": "null"
         },
         "information4":{
-        "type": "string",
-        "title": "Availible tty (nmea0183) interfaces of the MCS-Board:",
-        "enum": ttyinterfaces
+            "title": "Availible tty (nmea0183) interfaces of the MCS-Board:",
+            "description":`${ttyinterfaces}`,
+            "type": "null"
         },
         "information5":{
-        "description":"after change something below, restart your pi to make changes work:",
-        "type": "string",
-        "title": " ",
-        "default": "sudo reboot",
-        "readOnly": true
+            "title": "after change something below, restart your pi to make changes work:",
+            "description":"sudo reboot",
+            "type": "null"
         }
       }}
   )
@@ -109,6 +107,7 @@ module.exports = function (app) {
 
 
   plugin.start = function (options) {
+
     var asdstate = new Gpio(5, 'in');
     //script for autoshutdown
     function checkasd(){
