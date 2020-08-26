6ed7964035614c4f81333d437dd1bf5da2312d0e
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2020-08-26 20:15:54 +0200

    added 1-wire

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2020 Thomas Gersmann - GeDaD <t.gersmann@gedad.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const { exec } = require("child_process");
-const fs = require('fs')
+const fs = require('fs');
 var Gpio = require('onoff').Gpio;
 var  ttyinterfaces = [];
 var sensors = [];
+var speckeys =["environment.inside.engineRoom.temperature","environment.inside.freezer.temperature","environment.inside.heating.temperature","environment.inside.mainCabin.temperature","environment.inside.refrigerator.temperature","environment.inside.temperature","environment.outside.apparentWindChillTemperature","environment.outside.dewPointTemperature","environment.outside.heatIndexTemperature","environment.outside.temperature","environment.outside.theoreticalWindChillTemperature","environment.water.baitWell.temperature","environment.water.liveWell.temperature","environment.water.temperature,propulsion.*.coolantTemperature","propulsion.*.exhaustTemperature","propulsion.*.oilTemperature","propulsion.*.temperature","propulsion.*.transmission.oilTemperature"]; 
 
 module.exports = function (app) {
   let plugin = {}
@@ -15,50 +31,65 @@ module.exports = function (app) {
 
     //os config
     function sudoInstall(){
-        data = fs.readFileSync('/boot/config.txt', 'utf8');
-        var error = "no errors"
-        function execconfig(entry){
-            exec('{entry}', (error, stdout, stderr) => 
-            {
-            if (error) {
-                console.log(`MCS -> error: ${error.message}`);
-                error = `error ${error.message}`
-            }
-            if (stderr) {
-                console.log(`MCS -> stderr: ${stderr}`);
-                error= `error ${stderr}`
-            }
-            console.log(`MCS -> Added: ${entry} to system`);
-            })};
         
-        //install 1 sc16is752 overlay
-        if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600")==-1){
-            execconfig(`sudo sh -c \"echo 'dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600' >> /boot/config.txt\"`)
-        }
-        //install 2 sc16is752 overlay
-        if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600")==-1){
-            execconfig(`sudo sh -c \"echo 'dtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600' >> /boot/config.txt\"`)
-        }
-        //install 3 sc16is752 overlay
-        if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600")==-1){
-            execconfig(`sudo sh -c \"echo 'dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600' >> /boot/config.txt\"`)
-        }
-        //install mcp2515 overlay
-        if (data.indexOf("dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25")==-1){
-            execconfig(`sudo sh -c \"echo 'dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25' >> /boot/config.txt\"`)
-        }
-        //install spi-bcm2835
-        if (data.indexOf("dtoverlay=spi-bcm2835-overlay")==-1){
-            execconfig(`sudo sh -c \"dtoverlay=spi-bcm2835-overlay' >> /boot/config.txt\"`)
-        }
-        //add CAN0 device
-        var can0 = fs.readdirSync('/etc/network/interfaces.d/')
-        if (can0.includes("can0")==false) {
-            execconfig(`sudo sh -c \"echo '#physical can interfaces\\nallow-hotplug can0\\niface can0 can static\\nbitrate 250000\\ndown /sbin/ip link set $IFACE down\\nup /sbin/ifconfig $IFACE txqueuelen 10000' >> /etc/network/interfaces.d/can0\"`)
-    
-        }
-        return error
-    }
+      var error = "no errors"
+      function execconfig(entry){
+          exec(entry, (error, stdout, stderr) => 
+          {
+          if (error) {
+              console.log(`MCS -> error: ${error.message}`);
+              error = `error ${error.message}`
+          }
+          if (stderr) {
+              console.log(`MCS -> stderr: ${stderr}`);
+              error= `error ${stderr}`
+          }
+          console.log(`MCS -> Added: ${entry} to system`);
+          })};
+      var data = fs.readFileSync('/boot/config.txt', 'utf8');
+      
+      //install 1 sc16is752 overlay
+      if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600")==-1){
+          execconfig(`sudo sh -c \"echo 'dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600' >> /boot/config.txt\"`)
+      }
+      //install 2 sc16is752 overlay
+      if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600")==-1){
+          execconfig(`sudo sh -c \"echo 'dtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600' >> /boot/config.txt\"`)
+      }
+      //install 3 sc16is752 overlay
+      if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600")==-1){
+          execconfig(`sudo sh -c \"echo 'dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600' >> /boot/config.txt\"`)
+      }
+      //install mcp2515 overlay
+      if (data.indexOf("dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25")==-1){
+          execconfig(`sudo sh -c \"echo 'dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25' >> /boot/config.txt\"`)
+      }
+      //install spi-bcm2835
+      if (data.indexOf("dtoverlay=spi-bcm2835-overlay")==-1){
+          execconfig(`sudo sh -c \"echo 'dtoverlay=spi-bcm2835-overlay' >> /boot/config.txt\"`)
+      }
+      //add CAN0 device
+      var can0 = fs.readdirSync('/etc/network/interfaces.d/')
+      if (can0.includes("can0")==false) {
+          execconfig(`sudo sh -c \"echo '#physical can interfaces\\nallow-hotplug can0\\niface can0 can static\\nbitrate 250000\\ndown /sbin/ip link set $IFACE down\\nup /sbin/ifconfig $IFACE txqueuelen 10000' >> /etc/network/interfaces.d/can0\"`)
+      }
+      //added i2c-dev to /etc/modules:
+      var modules = fs.readFileSync('/etc/modules', 'utf8');
+      if (modules.includes("i2c_dev")==false) {
+        execconfig(`sudo sh -c \"echo 'i2c_dev' >> /etc/modules\"`)
+      }
+      //added ds2482 to /etc/modules:
+      if (modules.includes("i2c_dev")==false) {
+        execconfig(`sudo sh -c \"echo 'ds2482' >> /etc/modules\"`)
+      }
+      //added wire to /etc/modules:
+      if (modules.includes("i2c_dev")==false) {
+        execconfig(`sudo sh -c \"echo 'wire' >> /etc/modules\"`)
+      }
+      //loaded ds2482 device
+      execconfig(`echo 'ds2482 0x18' | sudo tee /sys/class/i2c-adapter/i2c-1/new_device`)
+      return error
+  }
     //read tty interfaces
     var files = fs.readdirSync('/dev/');
     files.forEach(check_ttydev);
@@ -104,9 +135,7 @@ module.exports = function (app) {
                 "description": `${sudoInstall()}`,
                 "type": "null"
             }}},
-        
-        "devices": {
-            
+        "devices": {     
             "type": "array",
             "title": "1-Wire Sensors (DS18B20)",
             "items": {
@@ -126,7 +155,7 @@ module.exports = function (app) {
                   "type": "string",
                   "title": "Signal K Key",
                   "description": "This is used to build the path in Signal K. It will be appended to environment",
-                  "enum": ["inside.engineroom.temperature"]
+                  "enum": speckeys
                 }
               }
             }
@@ -154,6 +183,21 @@ module.exports = function (app) {
     if (asdstate.readSync()==1 && options.active){
     timer = setInterval(checkasd, 3000);
     }
+
+    //1-wire Sensors send data
+    function readds18b20(){
+        var avdevices =options.devices
+        avdevices.forEach(readsensor)
+        function readsensor(getsensor){
+            var temp = fs.readFileSync("/sys/bus/w1/devices/"+ getsensor["oneWireId"] +"/w1_slave", 'utf8');
+            indext=temp.indexOf("t=")
+            temp= temp.slice(temp.indexOf("t=")+2,-1) / 1000 +273
+            console.log("signalKKey: "+getsensor["key"] + "    SensorID: "+getsensor["oneWireId"]+"    Value: "+temp)
+            var delta = createDeltaMessage (getsensor["key"], temp)
+            app.handleMessage(plugin.id, delta)
+        }
+    }
+    timerreadds18b20 = setInterval(readds18b20,3000)
     }
 
   plugin.stop = function () {
@@ -164,6 +208,29 @@ module.exports = function (app) {
     catch { console.log("Info: no asdstate working")}
   
   }
+
+
+  //create the delta message
+  function createDeltaMessage (key, value) {
+    return {
+      'context': 'vessels.' + app.selfId,
+      'updates': [
+        {
+          'source': {
+            'label': plugin.id
+          },
+          'timestamp': (new Date()).toISOString(),
+          'values': [
+            {
+              'path':  key,
+              'value': value
+            }
+          ]
+        }
+      ]
+    }
+  }
+
 
   return plugin
 }
