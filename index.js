65d338e380de0588e3cbb0bcba98cb150586a8b9
Teppo Kurki <teppo.kurki@iki.fi>
2020-09-06 21:13:03 +0300

    style: reformat
    Formatted with
    
    npx prettier --no-semi --write .

--- index.js
+++ index.js
@@ -12,196 +12,242 @@
  * limitations under the License.
  */
 
-const fs = require('fs');
-var Gpio = require('onoff').Gpio;
+const fs = require("fs")
+var Gpio = require("onoff").Gpio
 
-var  ttyinterfaces = [];
-var sensors = [];
+var ttyinterfaces = []
+var sensors = []
 //availible signalk-deltas
-var speckeys =["environment.inside.engineRoom.temperature","environment.inside.freezer.temperature","environment.inside.heating.temperature","environment.inside.mainCabin.temperature","environment.inside.refrigerator.temperature","environment.inside.temperature","environment.outside.apparentWindChillTemperature","environment.outside.dewPointTemperature","environment.outside.heatIndexTemperature","environment.outside.temperature","environment.outside.theoreticalWindChillTemperature","environment.water.baitWell.temperature","environment.water.liveWell.temperature","environment.water.temperature,propulsion.*.coolantTemperature","propulsion.*.exhaustTemperature","propulsion.*.oilTemperature","propulsion.*.temperature","propulsion.*.transmission.oilTemperature"]; 
-var error = [];
-let plugin = {};
-let timerreadds18b20 = null;
-let timer = null;
+var speckeys = [
+  "environment.inside.engineRoom.temperature",
+  "environment.inside.freezer.temperature",
+  "environment.inside.heating.temperature",
+  "environment.inside.mainCabin.temperature",
+  "environment.inside.refrigerator.temperature",
+  "environment.inside.temperature",
+  "environment.outside.apparentWindChillTemperature",
+  "environment.outside.dewPointTemperature",
+  "environment.outside.heatIndexTemperature",
+  "environment.outside.temperature",
+  "environment.outside.theoreticalWindChillTemperature",
+  "environment.water.baitWell.temperature",
+  "environment.water.liveWell.temperature",
+  "environment.water.temperature,propulsion.*.coolantTemperature",
+  "propulsion.*.exhaustTemperature",
+  "propulsion.*.oilTemperature",
+  "propulsion.*.temperature",
+  "propulsion.*.transmission.oilTemperature",
+]
+var error = []
+let plugin = {}
+let timerreadds18b20 = null
+let timer = null
 
 module.exports = function (app) {
   //check os entrys:
-  function check_entrys(){
-    var data = fs.readFileSync('/boot/config.txt', 'utf8');
-    if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600")==-1){
-    error.push("error dt-overlay sc16is752 0x4c")
+  function check_entrys() {
+    var data = fs.readFileSync("/boot/config.txt", "utf8")
+    if (
+      data.indexOf(
+        "dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600"
+      ) == -1
+    ) {
+      error.push("error dt-overlay sc16is752 0x4c")
     }
-    if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600")==-1){
+    if (
+      data.indexOf(
+        "dtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600"
+      ) == -1
+    ) {
       error.push("error dt-overlay sc16is752 0x49")
     }
-    if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=6,addr=0x48,xtal=14745600")==-1){
+    if (
+      data.indexOf(
+        "dtoverlay=sc16is752-i2c,int_pin=6,addr=0x48,xtal=14745600"
+      ) == -1
+    ) {
       error.push("error dt-overlay sc16is752 0x48")
     }
-    if (data.indexOf("dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25")==-1){
+    if (
+      data.indexOf("dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25") ==
+      -1
+    ) {
       error.push("error dt-overlay mcp2515")
     }
-    if (data.indexOf("dtoverlay=spi-bcm2835-overlay")==-1){
+    if (data.indexOf("dtoverlay=spi-bcm2835-overlay") == -1) {
       error.push("error dt-overlay spi bcm2835")
     }
-    var can0 = fs.readdirSync('/etc/network/interfaces.d/')
-    if (can0.includes("can0")==false){
+    var can0 = fs.readdirSync("/etc/network/interfaces.d/")
+    if (can0.includes("can0") == false) {
       error.push("error interfaces.d can0")
     }
-    var modules = fs.readFileSync('/etc/modules', 'utf8');
-    if (modules.includes("i2c_dev")==false) {
+    var modules = fs.readFileSync("/etc/modules", "utf8")
+    if (modules.includes("i2c_dev") == false) {
       error.push("error i2c_dev in modules")
     }
-    if (modules.includes("ds2482")==false) {
+    if (modules.includes("ds2482") == false) {
       error.push("error ds2482 in modules")
     }
-    if (modules.includes("wire")==false) {
+    if (modules.includes("wire") == false) {
       error.push("error wire in modules")
     }
-  return error
+    return error
   }
 
-  plugin.id = 'SignalK_raspberry_MCS'
-  plugin.name = 'Raspberry_MCS Plugin'
-  plugin.description = 'SignalK Plugin to provide MCS functionality to SignalK'
+  plugin.id = "SignalK_raspberry_MCS"
+  plugin.name = "Raspberry_MCS Plugin"
+  plugin.description = "SignalK Plugin to provide MCS functionality to SignalK"
 
-    //read tty interfaces
-    var files = fs.readdirSync('/dev/');
-    files.forEach(check_ttydev);
+  //read tty interfaces
+  var files = fs.readdirSync("/dev/")
+  files.forEach(check_ttydev)
 
-    function check_ttydev(item){
-      if (item.includes("ttySC")){
-          ttyinterfaces.push(item)
-      }
+  function check_ttydev(item) {
+    if (item.includes("ttySC")) {
+      ttyinterfaces.push(item)
     }
-    
-    //read 1-wire sensors
-    try{
-        var sensorx = fs.readdirSync('/sys/bus/w1/devices/'); ///sys/bus/w1/devices/
-    }
-    catch{
-        console.log("MCS => the 1-wire devices are not reachable")
-    }
-    sensorx.forEach(checkid);
+  }
 
-    function checkid(item){
-        if (item.slice(0,2)==28){
-            sensors.push(item)
-        }
-    }
+  //read 1-wire sensors
+  try {
+    var sensorx = fs.readdirSync("/sys/bus/w1/devices/") ///sys/bus/w1/devices/
+  } catch {
+    console.log("MCS => the 1-wire devices are not reachable")
+  }
+  sensorx.forEach(checkid)
 
-  plugin.schema =  () => (
-    {
-        "title": "Enable autoshutdown.",
-        "description": "Before you can start, execute the folowing command in a terminal! This installs all system config on your pi.",
-        "type": "object",
-        "required": ["oneWireId"],
-        "properties": {
-          "active": {
-            "type": "null",
-            "title": "sduo node $HOME/.signalk/node_modules/SignalK_raspberry_MCS/postinstall.js"
+  function checkid(item) {
+    if (item.slice(0, 2) == 28) {
+      sensors.push(item)
+    }
+  }
+
+  plugin.schema = () => ({
+    title: "Enable autoshutdown.",
+    description:
+      "Before you can start, execute the folowing command in a terminal! This installs all system config on your pi.",
+    type: "object",
+    required: ["oneWireId"],
+    properties: {
+      active: {
+        type: "null",
+        title:
+          "sduo node $HOME/.signalk/node_modules/SignalK_raspberry_MCS/postinstall.js",
+      },
+      setup: {
+        title: "Setup information",
+        type: "object",
+        properties: {
+          information1: {
+            title: "Availible tty (nmea0183) interfaces of the MCS-Board:",
+            description: `${ttyinterfaces}`,
+            type: "null",
+          },
+          information2: {
+            title: "If there is an error: (see also Server log):",
+            description: `${check_entrys()}`,
+            type: "null",
+          },
+        },
+      },
+      rate: {
+        title: "Sample Rate 1-wire Sensor (in seconds)",
+        description: "should be >1s each sensor!, min 10s",
+        type: "number",
+        default: 30,
+      },
+      devices: {
+        type: "array",
+        title: "1-Wire Sensors (DS18B20)",
+        items: {
+          type: "object",
+          properties: {
+            oneWireId: {
+              type: "string",
+              title: "Sensor Id",
+              enum: sensors,
             },
-        "setup":{
-          "title": "Setup information",
-          "type": "object",
-          "properties":{
-            "information1":{
-                "title": "Availible tty (nmea0183) interfaces of the MCS-Board:",
-                "description":`${ttyinterfaces}`,
-                "type": "null"
+            locationName: {
+              type: "string",
+              title: "Location name",
+              default: "Engine room",
             },
-            "information2":{
-                "title": "If there is an error: (see also Server log):",
-                "description": `${check_entrys()}`,
-                "type": "null"
-            }}},
-            "rate": {
-              "title": "Sample Rate 1-wire Sensor (in seconds)",
-              "description": "should be >1s each sensor!, min 10s",
-              "type": "number",
-              "default": 30
+            key: {
+              type: "string",
+              title: "Signal K Key",
+              description:
+                "This is used to build the path in Signal K. It will be appended to environment",
+              enum: speckeys,
             },
-        "devices": {     
-            "type": "array",
-            "title": "1-Wire Sensors (DS18B20)",
-            "items": {
-              "type": "object",
-              "properties": {
-                "oneWireId": {
-                  "type": "string",
-                  "title": "Sensor Id",
-                  "enum": sensors
-                },
-                "locationName": {
-                  "type": "string",
-                  "title": "Location name",
-                  "default": "Engine room"
-                },
-                "key": {
-                  "type": "string",
-                  "title": "Signal K Key",
-                  "description": "This is used to build the path in Signal K. It will be appended to environment",
-                  "enum": speckeys
-                }
-              }
-            }
-        }
-       }
-    } )
+          },
+        },
+      },
+    },
+  })
 
   plugin.start = function (options) {
-
-
     //1-wire Sensors send data
-    function readds18b20(){
-        var avdevices =options.devices
-        avdevices.forEach(readsensor)
-        function readsensor(getsensor){
-            try{
-                var temp = fs.readFileSync("/sys/bus/w1/devices/"+ getsensor["oneWireId"] +"/w1_slave", 'utf8');
-                indext=temp.indexOf("t=");
-                temp= temp.slice(temp.indexOf("t=")+2,-1) / 1000 +273;
-                app.debug("signalKKey: "+getsensor["key"] + "    SensorID: "+getsensor["oneWireId"]+"    Value: "+temp);
-                var delta = createDeltaMessage (getsensor["key"], temp);
-                app.handleMessage(plugin.id, delta);
-            }
-            catch{
-                console.log("MCS=> the configurated Sensor is not reachable:" +getsensor["oneWireId"])
-            }
+    function readds18b20() {
+      var avdevices = options.devices
+      avdevices.forEach(readsensor)
+      function readsensor(getsensor) {
+        try {
+          var temp = fs.readFileSync(
+            "/sys/bus/w1/devices/" + getsensor["oneWireId"] + "/w1_slave",
+            "utf8"
+          )
+          indext = temp.indexOf("t=")
+          temp = temp.slice(temp.indexOf("t=") + 2, -1) / 1000 + 273
+          app.debug(
+            "signalKKey: " +
+              getsensor["key"] +
+              "    SensorID: " +
+              getsensor["oneWireId"] +
+              "    Value: " +
+              temp
+          )
+          var delta = createDeltaMessage(getsensor["key"], temp)
+          app.handleMessage(plugin.id, delta)
+        } catch {
+          console.log(
+            "MCS=> the configurated Sensor is not reachable:" +
+              getsensor["oneWireId"]
+          )
         }
+      }
     }
-  var rate = options.rate
-  if (rate<10){
-    rate=10
+    var rate = options.rate
+    if (rate < 10) {
+      rate = 10
+    }
+    timerreadds18b20 = setInterval(readds18b20, rate * 1000)
   }
-  timerreadds18b20 = setInterval(readds18b20,rate*1000)
-  }
-
 
   plugin.stop = function () {
-     if(timerreadds18b20){
-       clearInterval(timerreadds18b20)}
+    if (timerreadds18b20) {
+      clearInterval(timerreadds18b20)
+    }
   }
 
   //create the delta message
-  function createDeltaMessage (key, value) {
+  function createDeltaMessage(key, value) {
     return {
-      'context': 'vessels.' + app.selfId,
-      'updates': [
+      context: "vessels." + app.selfId,
+      updates: [
         {
-          'source': {
-            'label': plugin.id
+          source: {
+            label: plugin.id,
           },
-          'timestamp': (new Date()).toISOString(),
-          'values': [
+          timestamp: new Date().toISOString(),
+          values: [
             {
-              'path':  key,
-              'value': value
-            }
-          ]
-        }
-      ]
+              path: key,
+              value: value,
+            },
+          ],
+        },
+      ],
     }
   }
   return plugin
 }
-
--- postinstall.js
+++ postinstall.js
@@ -15,74 +15,102 @@
 
 //installation script for MCS dt overlay and Modul entrys. Runs in sudo mode!
 const { exec } = require("child_process")
-const fs = require('fs');
+const fs = require("fs")
 
 sudoInstall()
-function sudoInstall(){
-        
-    function execconfig(entry){
-        exec(entry, (error, stdout, stderr) => 
-        {
-        if (error) {
-            console.log(`MCS -> error: ${error.message}`);
-            error = `error ${error.message}`
-        }
-        if (stderr) {
-            console.log(`MCS -> stderr: ${stderr}`);
-            error= `error ${stderr}`
-        }
-        console.log(`MCS -> Added: ${entry} to system`);
-        })};
-    var data = fs.readFileSync('/boot/config.txt', 'utf8');
-    
-    //install 1 sc16is752 overlay
-    if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600")==-1){
-        execconfig(`echo 'dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600' >> /boot/config.txt`)
-    }
-    //install 2 sc16is752 overlay
-    if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600")==-1){
-        execconfig(`echo 'dtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600' >> /boot/config.txt`)
-    }
-    //install 3 sc16is752 overlay
-    if (data.indexOf("dtoverlay=sc16is752-i2c,int_pin=6,addr=0x48,xtal=14745600")==-1){
-        execconfig(`echo 'dtoverlay=sc16is752-i2c,int_pin=6,addr=0x48,xtal=14745600' >> /boot/config.txt`)
-    }
-    //install mcp2515 overlay
-    if (data.indexOf("dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25")==-1){
-        execconfig(`echo 'dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25' >> /boot/config.txt`)
-    }
-    //install spi-bcm2835
-    if (data.indexOf("dtoverlay=spi-bcm2835-overlay")==-1){
-        execconfig(`echo 'dtoverlay=spi-bcm2835-overlay' >> /boot/config.txt`)
-    }
-    //add CAN0 device
-    var can0 = fs.readdirSync('/etc/network/interfaces.d/')
-    if (can0.includes("can0")==false) {
-        execconfig(`echo '#physical can interfaces\\nallow-hotplug can0\\niface can0 can static\\nbitrate 250000\\ndown /sbin/ip link set $IFACE down\\nup /sbin/ifconfig $IFACE txqueuelen 10000' >> /etc/network/interfaces.d/can0`)
-    }
-    //added i2c-dev to /etc/modules:
-    var modules = fs.readFileSync('/etc/modules', 'utf8');
-    if (modules.includes("i2c_dev")==false) {
-      execconfig(`echo 'i2c_dev' >> /etc/modules`)
-    }
-    //added ds2482 to /etc/modules:
-    if (modules.includes("ds2482")==false) {
-      execconfig(`echo 'ds2482' >> /etc/modules`)
-    }
-    //added wire to /etc/modules:
-    if (modules.includes("wire")==false) {
-      execconfig(`echo 'wire' >> /etc/modules`)
-    }
-    //create ds2482 device service
-    var service = fs.readdirSync('/etc/systemd/system/')
-    if (service.includes("mcsowire.service")==false){
-        execconfig("echo \"[Unit]\\nDescription=MCS owire start service\\nAfter=multi-user.target\\n\\n[Service]\\nType=simple\\nExecStart=/bin/sh -c \'echo ds2482 0x18 > /sys/bus/i2c/devices/i2c-1/new_device\'\\n\\n[Install]\\nWantedBy=multi-user.target\" | tee /etc/systemd/system/mcsowire.service")
-        execconfig("systemctl enable mcsowire.service")
-    }
-    //create MCS autoshutdown
-    if (service.includes("mcsasd.service")==false){
-        execconfig("echo \"[Unit]\\nDescription=MCS autoshutdown start service\\nAfter=multi-user.target\\n\\n[Service]\\nType=simple\\nExecStart=/usr/bin/python3 "+ __dirname +"/MCS-asd.py\\n\\n[Install]\\nWantedBy=multi-user.target\" | tee /etc/systemd/system/mcsasd.service")
-        execconfig("systemctl enable mcsasd.service")
-    }
-
+function sudoInstall() {
+  function execconfig(entry) {
+    exec(entry, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`MCS -> error: ${error.message}`)
+        error = `error ${error.message}`
+      }
+      if (stderr) {
+        console.log(`MCS -> stderr: ${stderr}`)
+        error = `error ${stderr}`
+      }
+      console.log(`MCS -> Added: ${entry} to system`)
+    })
   }
+  var data = fs.readFileSync("/boot/config.txt", "utf8")
+
+  //install 1 sc16is752 overlay
+  if (
+    data.indexOf(
+      "dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600"
+    ) == -1
+  ) {
+    execconfig(
+      `echo 'dtoverlay=sc16is752-i2c,int_pin=13,addr=0x4c,xtal=14745600' >> /boot/config.txt`
+    )
+  }
+  //install 2 sc16is752 overlay
+  if (
+    data.indexOf(
+      "dtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600"
+    ) == -1
+  ) {
+    execconfig(
+      `echo 'dtoverlay=sc16is752-i2c,int_pin=12,addr=0x49,xtal=14745600' >> /boot/config.txt`
+    )
+  }
+  //install 3 sc16is752 overlay
+  if (
+    data.indexOf("dtoverlay=sc16is752-i2c,int_pin=6,addr=0x48,xtal=14745600") ==
+    -1
+  ) {
+    execconfig(
+      `echo 'dtoverlay=sc16is752-i2c,int_pin=6,addr=0x48,xtal=14745600' >> /boot/config.txt`
+    )
+  }
+  //install mcp2515 overlay
+  if (
+    data.indexOf("dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25") ==
+    -1
+  ) {
+    execconfig(
+      `echo 'dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25' >> /boot/config.txt`
+    )
+  }
+  //install spi-bcm2835
+  if (data.indexOf("dtoverlay=spi-bcm2835-overlay") == -1) {
+    execconfig(`echo 'dtoverlay=spi-bcm2835-overlay' >> /boot/config.txt`)
+  }
+  //add CAN0 device
+  var can0 = fs.readdirSync("/etc/network/interfaces.d/")
+  if (can0.includes("can0") == false) {
+    execconfig(
+      `echo '#physical can interfaces\\nallow-hotplug can0\\niface can0 can static\\nbitrate 250000\\ndown /sbin/ip link set $IFACE down\\nup /sbin/ifconfig $IFACE txqueuelen 10000' >> /etc/network/interfaces.d/can0`
+    )
+  }
+  //added i2c-dev to /etc/modules:
+  var modules = fs.readFileSync("/etc/modules", "utf8")
+  if (modules.includes("i2c_dev") == false) {
+    execconfig(`echo 'i2c_dev' >> /etc/modules`)
+  }
+  //added ds2482 to /etc/modules:
+  if (modules.includes("ds2482") == false) {
+    execconfig(`echo 'ds2482' >> /etc/modules`)
+  }
+  //added wire to /etc/modules:
+  if (modules.includes("wire") == false) {
+    execconfig(`echo 'wire' >> /etc/modules`)
+  }
+  //create ds2482 device service
+  var service = fs.readdirSync("/etc/systemd/system/")
+  if (service.includes("mcsowire.service") == false) {
+    execconfig(
+      "echo \"[Unit]\\nDescription=MCS owire start service\\nAfter=multi-user.target\\n\\n[Service]\\nType=simple\\nExecStart=/bin/sh -c 'echo ds2482 0x18 > /sys/bus/i2c/devices/i2c-1/new_device'\\n\\n[Install]\\nWantedBy=multi-user.target\" | tee /etc/systemd/system/mcsowire.service"
+    )
+    execconfig("systemctl enable mcsowire.service")
+  }
+  //create MCS autoshutdown
+  if (service.includes("mcsasd.service") == false) {
+    execconfig(
+      'echo "[Unit]\\nDescription=MCS autoshutdown start service\\nAfter=multi-user.target\\n\\n[Service]\\nType=simple\\nExecStart=/usr/bin/python3 ' +
+        __dirname +
+        '/MCS-asd.py\\n\\n[Install]\\nWantedBy=multi-user.target" | tee /etc/systemd/system/mcsasd.service'
+    )
+    execconfig("systemctl enable mcsasd.service")
+  }
+}
